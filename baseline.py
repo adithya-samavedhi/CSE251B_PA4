9351be6eccd43eb5394f5d8025f72fe772f20ad3
adithya-samavedhi <adithya.samavedhi@gmail.com>
2023-03-07 03:47:27 +0000

    removing unneccessary lines from baseline and experiment.py

--- baseline.py
+++ baseline.py
@@ -7,31 +7,37 @@ import torchvision.models as models
 from torchvision import transforms, models
 
 #ToDO Fill in the __ values
-class TransferLearningResNet34(nn.Module):
+class TransferLearningResNet50(nn.Module):
 
-    def __init__(self, n_class, hidden_dim, vocab, embedding_size):
+    def __init__(self, n_class, hidden_dim, vocab, embedding_size, num_layers, model_type):
 
         super().__init__()
         self.vocab = vocab
         self.vocab_size = len(vocab)
         self.embed_size = embedding_size
         self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.model_type = model_type
         
         print(self.vocab_size)
 
-        resnet34 = models.resnet34(pretrained=True)
-        self.resnet34 = nn.Sequential(*list(resnet34.children())[:-1])
-        for param in self.resnet34.parameters():
+        resnet50 = models.resnet50(pretrained=True)
+        self.resnet50 = nn.Sequential(*list(resnet50.children())[:-1])
+        for param in self.resnet50.parameters():
             param.requires_grad = False
-        self.linear = nn.Linear(512, self.embed_size)
+        self.linear = nn.Linear(2048, self.embed_size)
         self.embedding = nn.Embedding(self.vocab_size, self.embed_size)
-        self.lstm = nn.LSTM(input_size=self.embed_size, hidden_size=self.hidden_dim, num_layers=2, batch_first=True)
+        
+        if self.model_type=="RNN":
+            self.decoder_unit = nn.RNN(input_size=self.embed_size, hidden_size=self.hidden_dim, num_layers=2, batch_first=True)
+        else:
+            self.decoder_unit = nn.LSTM(input_size=self.embed_size, hidden_size=self.hidden_dim, num_layers=2, batch_first=True)
         self.fc = nn.Linear(self.hidden_dim, self.vocab_size)
         self.softmax = nn.Softmax(dim=2)
 
 
     def forward(self, x, captions=None):
-        x = self.resnet34(x)  # shape (batch_size, 512, 1, 1)
+        x = self.resnet50(x)  # shape (batch_size, 512, 1, 1)
         x = x.squeeze()  # shape (batch_size, 512)
         x = self.linear(x)  # shape (batch_size, 256)
 
@@ -40,14 +46,14 @@ class TransferLearningResNet34(nn.Module):
         image_features = x.unsqueeze(1)
         embeddings = torch.cat((image_features, embeddings[:, :-1,:]), dim=1)
         
-        hiddens, c = self.lstm(embeddings)
+        hiddens, c = self.decoder_unit(embeddings)
         outputs = self.fc(hiddens)
         return outputs
     
         ## Greedy search 
     def generate_caption(self, inputs, deterministic=False, temperature=0.1):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        x = self.resnet34(inputs)  # shape (batch_size, 512, 1, 1)
+        x = self.resnet50(inputs)  # shape (batch_size, 512, 1, 1)
         x = x.squeeze()  # shape (batch_size, 512)
         x = self.linear(x)
         image_features = x.unsqueeze(1)
@@ -57,7 +63,7 @@ class TransferLearningResNet34(nn.Module):
 #         hidden = self.init_hidden(batch_size) # Get initial hidden state of the LSTM
     
         while True:
-            lstm_out, hidden = self.lstm(image_features) # lstm_out shape : (1, 1, hidden_size)
+            lstm_out, hidden = self.decoder_unit(image_features) # lstm_out shape : (1, 1, hidden_size)
             outputs = self.fc(lstm_out)  # outputs shape : (1, 1, vocab_size)
             outputs = outputs.squeeze(1) # outputs shape : (1, vocab_size)
             _, max_indice = torch.max(outputs, dim=1) # predict the most likely next word, max_indice shape : (1)
--- model_factory.py
+++ model_factory.py
@@ -1,11 +1,12 @@
 # Build and return the model here based on the configuration.
-from baseline import TransferLearningResNet34
+from baseline import TransferLearningResNet50
 def get_model(config_data, vocab):
     hidden_size = config_data['model']['hidden_size']
     embedding_size = config_data['model']['embedding_size']
     model_type = config_data['model']['model_type']
+    num_layers = config_data['model']['num_layers']
 
     # You may add more parameters if you want
-    model = TransferLearningResNet34(4, hidden_size,vocab, embedding_size)
+    model = TransferLearningResNet50(4, hidden_size,vocab, embedding_size, num_layers, model_type)
 
     return model
